0360038df2d800ee63be75c1aea7db269dc47f8e
soniadeca28 <sonia.deca@student.upt.ro>
2021-12-08 23:35:44 +0200

    Stored user's name and phone number and gave up the sign out after register

--- app/src/main/java/com/example/aplaceforpaws/RegisterActivity.java
+++ app/src/main/java/com/example/aplaceforpaws/RegisterActivity.java
@@ -1,30 +1,34 @@
 package com.example.aplaceforpaws;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    EditText email, password;
+    EditText email, password, name, phone;
     Button registerButton;
     FirebaseAuth auth;
-   ProgressBar progressBar;
+    ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String userId;
+   public static final String TAG = "TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +39,13 @@ public class RegisterActivity extends AppCompatActivity {
         back.setOnClickListener(v -> backToMainActivity());
         email = findViewById(R.id.registerEmail);
         password = findViewById(R.id.registerPassword);
+        name = findViewById(R.id.registerName);
+        phone = findViewById(R.id.registerPhone);
         registerButton = findViewById(R.id.registerButton);
         progressBar = findViewById(R.id.registerProgressBar);
 
         auth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
        /* if(auth.getCurrentUser() != null)
         {
@@ -47,8 +54,10 @@ public class RegisterActivity extends AppCompatActivity {
         }*/
 
         registerButton.setOnClickListener(v -> {
-            String eml = email.getText().toString().trim();
-            String psw = password.getText().toString().trim();
+            final String eml = email.getText().toString().trim();
+            final String psw = password.getText().toString().trim();
+            final String nm = name.getText().toString();
+            final String phn = phone.getText().toString();
 
             if(TextUtils.isEmpty(eml))
             {
@@ -74,8 +83,16 @@ public class RegisterActivity extends AppCompatActivity {
                 if(task.isSuccessful())
                 {
                     Toast.makeText(RegisterActivity.this,"Account created successfully",Toast.LENGTH_SHORT).show();
-                    FirebaseAuth.getInstance().signOut();
+                    // store the other data here
+                    userId = Objects.requireNonNull(auth.getCurrentUser()).getUid();
+                    DocumentReference documentReference = fStore.collection("users").document(userId);
+                    Map<String, String> user = new HashMap<>();
+                    user.put("name",nm);
+                    user.put("email",eml);
+                    user.put("phone number",phn);
+                    documentReference.set(user).addOnSuccessListener(unused -> Log.d(TAG,"onSuccess: user Profile is created for : " + userId)).addOnFailureListener(e -> Log.d(TAG, "onFailure: " + e.toString()));
                     startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                    //FirebaseAuth.getInstance().signOut();
                     finish();
                 }
                 else
