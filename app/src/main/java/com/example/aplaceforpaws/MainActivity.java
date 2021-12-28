b3a146f31885ae54979d61626b5cae57e025789e
soniadeca28 <sonia.deca@student.upt.ro>
2021-12-29 00:54:01 +0200

    Improved so if the user is logged in the app will open directly on intermediate page

--- app/src/main/java/com/example/aplaceforpaws/MainActivity.java
+++ app/src/main/java/com/example/aplaceforpaws/MainActivity.java
@@ -6,12 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), IntermediateActivity.class)); // sa te duca la pagina la care vei fi cand esti logat
+            finish();
+        }
 
         Button login = findViewById(R.id.loginPageButton);
         login.setOnClickListener(v -> openLoginPageActivity());
